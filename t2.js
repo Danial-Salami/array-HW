fc7f49e90d07828d8cf6e025f5b3aff023982a0a
Danial S <danialsari13@gmail.com>
2021-04-15 17:30:29 +0430

    fix t2

--- t2.js
+++ t2.js
@@ -1,10 +1,10 @@
 /*a function to combine two or more arrays and sort the final array*/
 let mainArr = []
-var hege = ["Cecilie", "Lone"];
-var stale = ["Emil", "Tobias", "Linus"];
-var kai = ["Robin"];
+var hege = ["1", "Lone"];
+var stale = ["Emil", "2", "Linus"];
+var kai = ["10"];
 
 function func(...arr) {
-    return mainArr.concat(...arr).sort();
+    return mainArr.concat(...arr).sort((a, b) => a < b);
 }
 console.log(func(hege, stale, kai))
